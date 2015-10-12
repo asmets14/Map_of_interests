6b678f727d1071414779e4787bff9c330c973382
Smets Angie <angie.d.n.smets@gmail.com>
2015-10-12 12:59:15 +0200

    Implements admin part
    Squashed commit of the following:
    
    commit b0d95bd8e1862cc725abd6ff7d59aa1a552268cc
    Author: Smets Angie <angie.d.n.smets@gmail.com>
    Date:   Mon Oct 12 12:58:20 2015 +0200
    
        delete
    
    commit 2632fedb4db0bd45215148635c52f01a6f369e30
    Author: Smets Angie <angie.d.n.smets@gmail.com>
    Date:   Mon Oct 12 12:56:42 2015 +0200
    
        implements admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root 'static_pages#homepage'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
