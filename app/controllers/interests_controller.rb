f04b12f058c2628caebc418c65089c2dedd628e6
Smets Angie <angie.d.n.smets@gmail.com>
2015-10-14 08:37:52 +0200

    adds map and filter
    Squashed commit of the following:
    
    commit e4df22bb8773d8b1a28517467f2761b05826f0a2
    Author: Smets Angie <angie.d.n.smets@gmail.com>
    Date:   Wed Oct 14 08:36:48 2015 +0200
    
        wip
    
    commit c842c8e4fb90a900000ebc76e54648a68d7c2368
    Author: Smets Angie <angie.d.n.smets@gmail.com>
    Date:   Tue Oct 13 22:42:36 2015 +0200
    
        javascript filter for checkbox display
    
    commit bb39d7f3d273e0f98fc7316adeb270682c2f421b
    Author: Smets Angie <angie.d.n.smets@gmail.com>
    Date:   Tue Oct 13 13:59:42 2015 +0200
    
        adds map of google api

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -2,4 +2,14 @@ class InterestsController < ApplicationController
   def index
     @interest = Interest.all
   end
+  
+  def show
+    if params[:category] == 'All'
+      @interest = Interest.all
+    else
+      @interest = Interest.where(category: params[:category])
+    end
+    data = {:message => @interest}
+    render :json => data, :status => :ok
+    end
 end
