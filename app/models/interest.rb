f2c401026ef8e74856b9912ed44c94e37f8dd4b5
Smets Angie <angie.d.n.smets@gmail.com>
2015-10-26 11:29:12 +0100

    test

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,9 +1,15 @@
 class Interest < ActiveRecord::Base
   CATEGORY = %w(Theatre Club Museum Restaurant Bar Cafe)
   geocoded_by :address
-  before_validation :geocode
+  before_validation :test
   validates :address, presence: true
   validates :name, presence: true
   validates :latitude, presence: true
   validates :longitude, presence: true
+
+  def test
+    if :longitude.nil?
+     :geocode
+    end
+  end
 end
