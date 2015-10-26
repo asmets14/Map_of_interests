08c386b5fa7c7ee61578c4a2fb7a96a3086dabd3
Smets Angie <angie.d.n.smets@gmail.com>
2015-10-26 11:41:59 +0100

    if stattement for geocoder

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,15 +1,15 @@
 class Interest < ActiveRecord::Base
   CATEGORY = %w(Theatre Club Museum Restaurant Bar Cafe)
   geocoded_by :address
-  before_validation :test
+  before_validation :activate_geocoder
   validates :address, presence: true
   validates :name, presence: true
   validates :latitude, presence: true
   validates :longitude, presence: true
 
-  def test
-    if :longitude.nil?
-     :geocode
+  def activate_geocoder
+    if longitude.nil? and latitude.nil?
+      geocode
     end
   end
 end
