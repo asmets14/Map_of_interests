df8792f7afba6021f68fe178f6a9e088adb81efa
Smets Angie <angie.d.n.smets@gmail.com>
2015-10-26 11:42:33 +0100

    adds if statement for geocoder
    Squashed commit of the following:
    
    commit 08c386b5fa7c7ee61578c4a2fb7a96a3086dabd3
    Author: Smets Angie <angie.d.n.smets@gmail.com>
    Date:   Mon Oct 26 11:41:59 2015 +0100
    
        if stattement for geocoder
    
    commit f2c401026ef8e74856b9912ed44c94e37f8dd4b5
    Author: Smets Angie <angie.d.n.smets@gmail.com>
    Date:   Mon Oct 26 11:29:12 2015 +0100
    
        test

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,9 +1,15 @@
 class Interest < ActiveRecord::Base
   CATEGORY = %w(Theatre Club Museum Restaurant Bar Cafe)
   geocoded_by :address
-  before_validation :geocode
+  before_validation :activate_geocoder
   validates :address, presence: true
   validates :name, presence: true
   validates :latitude, presence: true
   validates :longitude, presence: true
+
+  def activate_geocoder
+    if longitude.nil? and latitude.nil?
+      geocode
+    end
+  end
 end
