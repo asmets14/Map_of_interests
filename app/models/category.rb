40de8e1379c64d05671ca23502a5a3c489287f6c
Smets Angie <angie.d.n.smets@gmail.com>
2015-11-26 16:43:09 +0100

    wip

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,13 +1,13 @@
 class Category < ActiveRecord::Base
   has_attached_file :thumbnail,
-                    styles: { medium: "35x35#", thumb: "100x100#" } ,
+                    styles: { medium: "35x35#", thumb: "35x35#" } ,
                     default_url: 'https://storage.googleapis.com/geolocalisation/missing.png'
   validates_attachment_content_type :thumbnail, content_type: /\Aimage\/.*\Z/
 
   def decorate
     {
       :name => name,
-      :icon_url => thumbnail.url(:medium)
+      :icon_url => thumbnail.url(:thumb)
     }
   end
 end
