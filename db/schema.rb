b0e54de274f754a3c4551521895631f2b00df956
Smets Angie <angie.d.n.smets@gmail.com>
2015-12-01 16:19:23 +0100

    remove bookmarkers on schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201115920) do
+ActiveRecord::Schema.define(version: 20151201151635) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -45,13 +45,6 @@ ActiveRecord::Schema.define(version: 20151201115920) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
-
-  create_table "bookmarkers", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "interest_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "bookmarks", force: true do |t|
     t.integer  "user_id"
