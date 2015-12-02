4e377737f7193fa0d5dbc931447810bb9d767660
Smets Angie <angie.d.n.smets@gmail.com>
2015-12-02 10:17:42 +0100

    l

---
+++ app/models/interest.rb
@@ -0,0 +1,23 @@
+class Interest < ActiveRecord::Base
+  has_many :bookmarkers
+  has_many :users, through: :bookmarkers
+  belongs_to :category
+  acts_as_taggable
+  geocoded_by :address
+  scope :published, -> {PointOfInterest.where(:published => true)}
+  before_validation :activate_geocoder
+  validates :address, presence: true
+  validates :name, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  has_attached_file :thumbnail,
+                    styles: { medium: "300x300#", thumb: "100x100#" } ,
+                    default_url: 'https://storage.googleapis.com/geolocalisation/missing.png'
+  validates_attachment_content_type :thumbnail, content_type: /\Aimage\/.*\Z/
+
+  def activate_geocoder
+    if longitude.nil? and latitude.nil?
+      geocode
+    end
+  end
+end
