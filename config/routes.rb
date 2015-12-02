787919620490fc4bd719875d93a30f493695d754
Smets Angie <angie.d.n.smets@gmail.com>
2015-12-02 09:45:44 +0100

    point od interest doens work

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   devise_for :users,
                  :controllers => { :registrations => 'users/registrations' }
 
-  resources :interests
+  resources :pointOfInterests
   resources :categories
   resources :bookmarks
-  root 'interests#fullscreen'
+  root 'PointOfInterests#fullscreen'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
